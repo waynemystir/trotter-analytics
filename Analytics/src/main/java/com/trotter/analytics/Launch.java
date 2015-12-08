667aca137ae1128c1af2f021d0a0d582f5005826
Wayne Small <waynemystir@gmail.com>
2015-12-08 11:45:04 -0500

    more work to incorporate AdWords

--- Analytics/src/main/java/com/trotter/analytics/Launch.java
+++ Analytics/src/main/java/com/trotter/analytics/Launch.java
@@ -13,23 +13,36 @@ public class Launch {
   @Id public Long id;
   @Index public Date date;
   @Index public Boolean newInstall;
+  @Index public Boolean adWordsInstall;
 
   public String ipAddress;
   public String osType;
   public String osVersion;
   public String deviceType;
+  public String iosIdfa;
+  public String bundleId;
+  public String bundleVersion;
+  public String appVersion;
+  public Boolean limitAdTracking;
 
   public Launch() {
     date = new Date();
   };
 
-  public Launch(String ipAddress, String osType, String osVersion, String deviceType, Boolean newInstall) {
+  public Launch(String ipAddress, String osType, String osVersion, String deviceType, Boolean newInstall, Boolean adWordsInstall,
+                  String iosIdfa, String bundleId, String bundleVersion, String appVersion, Boolean limitAdTracking) {
     this();
     this.ipAddress = ipAddress;
     this.osType = osType;
     this.osVersion = osVersion;
     this.deviceType = deviceType;
     this.newInstall = newInstall;
+    this.adWordsInstall = adWordsInstall;
+    this.iosIdfa = iosIdfa;
+    this.bundleId = bundleId;
+    this.bundleVersion = bundleVersion;
+    this.appVersion = appVersion;
+    this.limitAdTracking = limitAdTracking;
   }
 
   public String getIpAddress() {
@@ -70,5 +83,53 @@ public class Launch {
 
   public void setNewInstall(Boolean newInstall) {
     this.newInstall = newInstall;
+  }
+
+  public Boolean getAdWordsInstall() {
+    return adWordsInstall;
+  }
+
+  public void setAdWordsInstall(Boolean adWordsInstall) {
+    this.adWordsInstall = adWordsInstall;
+  }
+
+  public String getIosIdfa() {
+    return iosIdfa;
+  }
+
+  public void setIosIdfa(String iosIdfa) {
+    this.iosIdfa = iosIdfa;
+  }
+
+  public String getBundleId() {
+    return bundleId;
+  }
+
+  public void setBundleId(String bundleId) {
+    this.bundleId = bundleId;
+  }
+
+  public String getBundleVersion() {
+    return bundleVersion;
+  }
+
+  public void setBundleVersion(String bundleVersion) {
+    this.bundleVersion = bundleVersion;
+  }
+
+  public String getAppVersion() {
+    return appVersion;
+  }
+
+  public void setAppVersion(String appVersion) {
+    this.appVersion = appVersion;
+  }
+
+  public Boolean getLimitAdTracking() {
+    return limitAdTracking;
+  }
+
+  public void setLimitAdTracking(Boolean limitAdTracking) {
+    this.limitAdTracking = limitAdTracking;
   }
 }
--- Analytics/src/main/java/com/trotter/analytics/TrotterAnalytics.java
+++ Analytics/src/main/java/com/trotter/analytics/TrotterAnalytics.java
@@ -4,8 +4,21 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.response.NotFoundException;
 import com.google.appengine.api.users.User;
+import com.google.appengine.api.ThreadManager;
+ 
+import com.google.appengine.api.urlfetch.HTTPHeader;
+import com.google.appengine.api.urlfetch.HTTPResponse;
+import com.google.appengine.api.urlfetch.URLFetchService;
+import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+ 
+import com.google.appengine.api.urlfetch.HTTPHeader;
 
 import java.lang.Float;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
@@ -14,6 +27,14 @@ import javax.inject.Named;
 import com.googlecode.objectify.ObjectifyService;
 
 import java.util.logging.Logger;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Defines v1 of a Trotter Analytics
@@ -56,6 +77,16 @@ public class TrotterAnalytics {
     trotterProblemResponse.put("apiMethod", "postTrotterProblem");
   }
 
+  private static final String ADWORDS_URL = "https://www.googleadservices.com/pagead/conversion";
+  private static final String ADWORDS_CONVERSION_ID = "12345";
+  private static final String ADWORDS_CONVERSION_LABEL = "waynester";
+
+  private static final String ADWORDS_PK_LABEL = "label";
+  private static final String ADWORDS_PK_RDID = "rdid";
+  private static final String ADWORDS_PK_BUNDLEID = "bundleid";
+  private static final String ADWORDS_PK_IDTYPE = "idtype";
+  private static final String ADWORDS_PK_LIMIT_AD_TRACKING_STATUS = "lat";
+
   private Boolean isValidKey(String apiKey) {
       Boolean rb = apiKey.equals(Constants.TROTTER_API_KEY);
 
@@ -71,15 +102,64 @@ public class TrotterAnalytics {
                               @Named("osType") String osType,
                               @Named("osVersion") String osVersion,
                               @Named("deviceType") String deviceType,
-                              @Named("newInstall") Boolean newInstall) {
+                              @Named("newInstall") Boolean newInstall,
+                              @Named("iosIdfa") String iosIdfa,
+                              @Named("limitAdTracking") Boolean limitAdTracking,
+                              @Named("bundleId") String bundleId,
+                              @Named("bundleVersion") String bundleVersion,
+                              @Named("appVersion") String appVersion) {
 
     if (!isValidKey(apiKey)) return null;
 
-    Launch launch = new Launch(ipAddress, osType, osVersion, deviceType, newInstall);
+    Launch launch = new Launch(ipAddress, osType, osVersion, deviceType, newInstall, false, iosIdfa, bundleId, bundleVersion,
+                                  appVersion, limitAdTracking);
     ObjectifyService.ofy().save().entity(launch).now();
 
+    checkIfAdWordsInstall(launch);
     return launchResponse;
   }
+
+  protected void checkIfAdWordsInstall(Launch launch) {
+        log.info("START checkIfAdWordsInstall: " + launch.id);
+        URLFetchService fetcher = URLFetchServiceFactory.getURLFetchService();
+        try {
+            // URL url = new URL("https://api.ipify.org");
+
+            String us = String.format("%s/%s/?%s=%s&%s=%s&%s=%s&%s=%b", ADWORDS_URL, ADWORDS_CONVERSION_ID, ADWORDS_PK_LABEL,
+                                                    ADWORDS_CONVERSION_LABEL, ADWORDS_PK_RDID, launch.iosIdfa,
+                                                    ADWORDS_PK_IDTYPE, "idfa", ADWORDS_PK_LIMIT_AD_TRACKING_STATUS,
+                                                    launch.limitAdTracking);
+            log.info("checkIfAdWordsInstall URLstr: " + us);
+            URL url = new URL(us);
+            Future future = fetcher.fetchAsync(url);
+ 
+            // Other stuff can happen here!
+ 
+            HTTPResponse response = (HTTPResponse) future.get();
+            byte[] content = response.getContent();
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            bos.write(content);
+            String responseString = new String(bos.toByteArray());
+            int responseCode = response.getResponseCode();
+            log.info(String.format("caw code:%d rs:%s", responseCode, responseString));
+            URL finalUrl = response.getFinalUrl();
+            List headers = response.getHeaders();
+ 
+            // for(HTTPHeader header : headers) {
+            //     String headerName = header.getName();
+            //     String headerValue = header.getValue();
+            // }
+ 
+        } catch (IOException e) {
+            // new URL throws MalformedUrlException, which is impossible for us here
+        } catch (InterruptedException e) {
+            // Exception from using java.concurrent.Future
+        } catch (ExecutionException e) {
+            // Exception from using java.concurrent.Future
+            e.printStackTrace();
+        }
+ 
+    }
 
   @ApiMethod(name = "postHotelSearch")
   public Hashtable postHotelSearch(@Named("apiKey") String apiKey,
